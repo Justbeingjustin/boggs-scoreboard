b59ea5f06d658d42999efbb38030951317aaf8d9
Justin Gerber <Justbeingjustin@aol.com>
2024-04-25 20:39:48 -0700

    changes

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,23 @@
+import CopyPlugin from 'copy-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    webpack5: true,
+
+    webpack: function (config, { dev, isServer }) {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) config.resolve.fallback.fs = false
+
+        // copy files you're interested in
+        if (!dev) {
+            config.plugins.push(
+                new CopyPlugin({ patterns: [{ from: 'fonts', to: 'fonts' }] })
+            )
+        }
+
+        return config
+    }
+};
 
 export default nextConfig;
