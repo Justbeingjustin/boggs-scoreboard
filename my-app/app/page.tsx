e52eca516559b85adbe9ce85cc501a9267572395
Justin Gerber <Justbeingjustin@aol.com>
2024-04-25 23:13:44 -0700

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -66,7 +66,7 @@ export default function Component() {
 
 
 
-      alert('Scoreboard created successfully');
+      //alert('Scoreboard created successfully');
       const cjs = new Castjs();
       if (cjs.available) {
         cjs.cast(payload.url, {
@@ -86,6 +86,7 @@ export default function Component() {
         console.log('Casting is not available');
       }
     } catch (error) {
+
       console.error(error);
       alert('Failed to create scoreboard');
     }
